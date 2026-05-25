ebc4d314be037362f0ab2aa6f9402877a2729cd4
Vegil Camilon <camilonvegil@gmail.com>
2026-05-25 21:01:34 +0800

    Test failed 2

--- apps/e2e/tests/pages/NavBar.ts
+++ apps/e2e/tests/pages/NavBar.ts
@@ -18,7 +18,7 @@ export class NavBar {
   }
 
   brandLink(): Locator {
-    return this.page.getByRole('link', { name: '💸 Expenses' });
+    return this.page.getByRole('link', { name: '💸 Expesiveness' });
   }
 
   footer(): Locator {
